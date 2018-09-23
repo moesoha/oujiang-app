e49787b989395a4469dfe3e7de2ae15933d93de3
Soha Jin <soha@lohu.info>
2018-09-23 18:40:10 +0800

    A successful demo

---
+++ OujiangSchedule/OujiangSchedule/ViewModels/LessonListViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using Tianhai.OujiangApp.Schedule.Models;
+using Tianhai.OujiangApp.Schedule.Views;
+
+namespace Tianhai.OujiangApp.Schedule.ViewModels{
+	public class LessonListViewModel:BaseViewModel{
+		public ObservableCollection<Models.Lesson> Lessons{get;set;}
+		public Command LoadLessonsCommand{get;set;}
+
+		public LessonListViewModel(){
+			Title="课程列表";
+			Lessons=new ObservableCollection<Models.Lesson>();
+			LoadLessonsCommand=new Command(async ()=>await ExecuteLoadCommand());
+		}
+
+		async Task ExecuteLoadCommand(){
+			if(IsBusy){
+				return;
+			}
+
+			IsBusy=true;
+			try{
+				Lessons.Clear();
+				var lessons=await Services.ScheduleService.fetchCurrent("demo");
+				lessons.Lessons.ForEach(o=>{
+					Lessons.Add(o);
+				});
+			}catch(Exception ex){
+				Debug.WriteLine(ex);
+			}finally{
+				IsBusy=false;
+			}
+		}
+	}
+}
---
+++ OujiangSchedule/OujiangSchedule/Views/LessonListPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Tianhai.OujiangApp.Schedule.Models;
+using Tianhai.OujiangApp.Schedule.Views;
+using Tianhai.OujiangApp.Schedule.ViewModels;
+
+namespace Tianhai.OujiangApp.Schedule.Views{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LessonListPage:ContentPage{
+		LessonListViewModel viewModel;
+
+		public LessonListPage(){
+			InitializeComponent();
+
+			BindingContext=viewModel=new LessonListViewModel();
+		}
+
+		protected override void OnAppearing(){
+			base.OnAppearing();
+
+			if(viewModel.Lessons.Count==0){
+				viewModel.LoadLessonsCommand.Execute(null);
+			}
+		}
+	}
+}
