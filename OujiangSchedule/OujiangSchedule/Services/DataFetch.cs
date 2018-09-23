f1acafb4d551fd3c861b429ee42e39332bb78033
Soha Jin <soha@lohu.info>
2018-09-23 12:03:43 +0800

    orz

---
+++ OujiangSchedule/OujiangSchedule/Services/DataFetch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Tianhai.OujiangApp.Schedule.Services{
+	public class DataFetch{
+		
+	}
+}
